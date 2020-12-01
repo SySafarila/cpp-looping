a96d0fb52b249316b7b754348ce9b2244486e6b0
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2020-12-01 07:13:58 +0700

    Add files via upload

---
+++ 20201201 loop - do while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 1;
+	
+	cout << "Looping enggunakan Do While." << endl;
+	cout << endl;
+	do {
+		cout << i << "\n";
+		i++;
+	}
+	while(i <= 10);
+	
+	return 0;
+}
---
+++ 20201201 loop - while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i = 1;
+	cout << "Looping menggunakan While." << endl;
+	cout << endl;
+	while (i <= 10) {
+		cout << i << "\n";
+		i++;
+	}
+	return 0;
+}
